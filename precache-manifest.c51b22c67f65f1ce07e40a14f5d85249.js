a386c34fc149c0b91754e9ccdc44d19a84bf4a14
Terry Mun <hello@terrymun.com>
2019-04-29 20:35:52 +0200

    Initial dist substree commit

---
+++ precache-manifest.c51b22c67f65f1ce07e40a14f5d85249.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a8eff6f52ebe76efd2d",
+    "url": "/css/app.37ae5090.css"
+  },
+  {
+    "revision": "9a8eff6f52ebe76efd2d",
+    "url": "/js/app.b90a6813.js"
+  },
+  {
+    "revision": "84eb7a18cb79bc3cdebc",
+    "url": "/js/chunk-vendors.36630957.js"
+  },
+  {
+    "revision": "4cdf6bca40c278123a4a",
+    "url": "/css/icon-styling.e2efd99c.css"
+  },
+  {
+    "revision": "4cdf6bca40c278123a4a",
+    "url": "/js/icon-styling.bffe0145.js"
+  },
+  {
+    "revision": "a0faaf4e8ab123b88fbf",
+    "url": "/js/theme0.4a2d369f.js"
+  },
+  {
+    "revision": "48b1663f1d50a46bcf8e",
+    "url": "/js/theme1.4d621d05.js"
+  },
+  {
+    "revision": "07cb3ebc4733b7e3d8e9",
+    "url": "/js/theme2.335fe238.js"
+  },
+  {
+    "revision": "fc86436b4d8b9579232e",
+    "url": "/js/theme4.6bec35bc.js"
+  },
+  {
+    "revision": "3da350c04408c718c55e",
+    "url": "/css/themeing.c9689648.css"
+  },
+  {
+    "revision": "3da350c04408c718c55e",
+    "url": "/js/themeing.3d091914.js"
+  },
+  {
+    "revision": "a300f16e61f2b353a1a774f62d3c46ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.c51b22c67f65f1ce07e40a14f5d85249.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vuejs-with-css-variables"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
